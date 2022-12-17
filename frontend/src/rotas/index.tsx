481c3e858f97f30fb607ab02170d44611c916cc7
Christian de Oliveira <christianoliveira1908@hotmail.com>
2022-12-17 10:53:19 -0300

    created MyAccount page

--- frontend/src/rotas/index.tsx
+++ frontend/src/rotas/index.tsx
@@ -1,14 +1,18 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "../paginas/Home"
+import MyAccount from "../paginas/MinhaConta"
 import PaginaBase from "../paginas/PaginaBase"
 
 
 const Rotas = () => {
-    return (<Routes>
-      <Route path='/' element={<PaginaBase />}>
-        <Route path='/' element={<Home />} />
-      </Route>
-    </Routes>)
+  return (<Routes>
+    <Route path='/' element={<PaginaBase />}>
+      <Route path='/' element={<Home />} />
+    </Route>
+    <Route path='minha-conta' element={<PaginaBase />}>
+      <Route path='pedidos' element={<MyAccount />} />
+    </Route>
+  </Routes>)
 }
 
 export default Rotas
