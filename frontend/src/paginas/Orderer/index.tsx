5a3198ca6dbed4a62450440ee4db0c73fa457031
Christian de Oliveira <christianoliveira1908@hotmail.com>
2022-12-21 08:14:32 -0300

    stylization Orderer page;

--- frontend/src/paginas/Orderer/index.tsx
+++ frontend/src/paginas/Orderer/index.tsx
@@ -1,14 +1,33 @@
 import { AbBotao } from "ds-alurabooks";
+import style from "./style.module.css";
 
 const Orderer = () => {
     return (
-        <section className="pedidos">
+        <section className={style.OrdererContainer}>
             <h1>
                 Meus pedidos
             </h1>
-            <div>
+            <div >
                 <ul>
-                    <li>
+                    <li className={style.Orderer}>
+                        <div>
+                            <p>Pedido: <strong>124567</strong></p>
+                            <p>Data do pedido: <strong>17/12/2022</strong></p>
+                            <p>Valor total: <strong>R$ 42.00</strong></p>
+                            <p>Entrega realizada em: <strong>18/12/2022</strong></p>
+                        </div>
+                        <AbBotao texto="Detalhes" />
+                    </li>
+                    <li className={style.Orderer}>
+                        <div>
+                            <p>Pedido: <strong>124567</strong></p>
+                            <p>Data do pedido: <strong>17/12/2022</strong></p>
+                            <p>Valor total: <strong>R$ 42.00</strong></p>
+                            <p>Entrega realizada em: <strong>18/12/2022</strong></p>
+                        </div>
+                        <AbBotao texto="Detalhes" />
+                    </li>
+                    <li className={style.Orderer}>
                         <div>
                             <p>Pedido: <strong>124567</strong></p>
                             <p>Data do pedido: <strong>17/12/2022</strong></p>
